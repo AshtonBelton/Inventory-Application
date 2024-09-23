bc4cc13eda751b1e9d18a240eb387e3f3c6b34cc
Ashton Belton <ashbelton24@gmail.com>
2024-09-22 19:31:14 -0600

    Added layout and css styling for the application

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const db = require('./config/db');
+const path = require('path');
 
 app.use(express.json());
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Get all Categories 
 app.get('/categories', async (req, res) => {
